0ad16c6f59f92f842d0a8d0ecb0198815a5f1941
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-06 15:13:53 +0530

    feat: intialized solana libraries and added connect wallet button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,43 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { clusterApiUrl } from '@solana/web3.js'
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const network = WalletAdapterNetwork.Devnet
+
+  const endpoint = clusterApiUrl(network)
+
+  const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter({ network }),
+  ]
+
   return (
-    <Layout>
-      <Head>
-        <title>Not Apple Inc</title>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Layout>
+            <Head>
+              <title>Not Apple Inc</title>
+            </Head>
+            <Component {...pageProps} />
+          </Layout>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
 
 export default function HomePage() {
+  const { publicKey } = useWallet()
+
   return (
     <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
       <SiteHeading>Not Apple Inc</SiteHeading>
 
-      <Products submitTarget="/checkout" enabled={true} />
+      <div className="basis-1/4">
+        <WalletMultiButton className="duration-i00 !bg-gray-900 transition hover:scale-110" />
+      </div>
+
+      <Products submitTarget="/checkout" enabled={publicKey != null} />
     </div>
   )
 }
